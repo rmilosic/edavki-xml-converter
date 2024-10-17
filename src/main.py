47c2980ec20cf4ed3a8b9a89308914cd5fe9f5b5
Rok Milošič <rok.milosic@gmail.com>
2024-10-17 20:37:03 +0200

    added singular buy/sell fifo combinations

--- src/main.py
+++ src/main.py
@@ -84,29 +84,43 @@ def process_fifo(args, config):
     
     
     # Function to handle buying stocks
-    def buy(quantity, price):
-        fifo_queue.append((quantity, price))
+    def buy(quantity, price, date):
+        fifo_queue.append((quantity, price, date))
 
     # Function to handle selling stocks and calculate profit/loss
-    def sell(quantity, sale_price):
+    def sell(quantity, sale_price, sell_date):
         #TODO: upgrade to track which buy orders (price, quantity, ID were used for a sale)
         total_cost = 0
-        sold_quantity = quantity
         total_proceeds = quantity * sale_price
+        
+        # sell table
+        sell_table = pd.DataFrame(columns=["date_sold", "sold_qty", "sold_price", "date_bought", "bought_qty", "bought_price"])
         while quantity > 0:
             oldest_stock = fifo_queue.popleft()
-            available_qty, price = oldest_stock
+            available_qty, buy_price, buy_date = oldest_stock
+            # store pairing
+            # date, sold stock, price, quantity
+            # matched stocks date, stock, price, matched quantity 
+            sell_table.loc[-1] = {
+                "date_sold": sell_date,
+                "sold_qty": quantity,
+                "sold_price": sale_price,
+                "date_bought": buy_date,
+                "bought_qty": available_qty,
+                "bought_price": buy_price
+            }
+            
             if quantity >= available_qty:
-                total_cost += available_qty * price
+                total_cost += available_qty * buy_price
                 quantity -= available_qty
             else:
-                total_cost += quantity * price
-                fifo_queue.appendleft((available_qty - quantity, price))
+                total_cost += quantity * buy_price
+                fifo_queue.appendleft((available_qty - quantity, buy_price, buy_date))
                 quantity = 0
                 
         # Calculate profit/loss
         profit_or_loss = total_proceeds - total_cost
-        return total_cost, total_proceeds, profit_or_loss
+        return total_cost, total_proceeds, profit_or_loss, sell_table
     # FIFO queue to hold purchased stocks
     
     degiro_data = parse_degiro_transactions_data(args.file_path, args.year)
@@ -117,15 +131,24 @@ def process_fifo(args, config):
         # datum, produkt, isin, počet, price, fifo cost, proceeds, profit/loss
         sales_records = []
         product_data = degiro_data[degiro_data["Produkt"] == product]
+        
+        sale_master_table = pd.DataFrame(columns=["product", "date_sold", "sold_qty", "sold_price", "date_bought", "bought_qty", "bought_price"])
+        
         for index, row in product_data.iterrows():
+            
             
             count = row["Počet"]
             price = row["Cena"]
+            date = row["Datum"]
             action = "buy" if count > 0 else "sell"
             if action == 'buy':
-                buy(count, price)
+                buy(count, price, date)
             elif action == 'sell':
-                total_cost, total_proceeds, profit_or_loss = sell(abs(count), price)
+                total_cost, total_proceeds, profit_or_loss, sell_table = sell(abs(count), price, date)
+                
+                sell_table["product"] = product
+                sale_master_table = pd.concat([sale_master_table, sell_table], axis=0, ignore_index=True)
+                
                 sales_records.append((row["Datum"], row["Produkt"], row["ISIN"], row["Počet"], row["Cena"], total_cost, total_proceeds, profit_or_loss))
                 print(f"Sold {count} shares:")
                 print(f"  FIFO Cost: €{total_cost}")
@@ -135,7 +158,7 @@ def process_fifo(args, config):
         
         fifo_sales = pd.DataFrame(columns=["Datum", "Produkt", "ISIN", "Počet", "Cena", "fifo cost", "proceeds", "profit/loss"], data=sales_records)
         fifo_sales.to_csv(f"degiro_fifo_{product}.csv", encoding='utf-8')
-    
+        sale_master_table.to_csv(f"degiro_fifo_detail_{product}.csv", encoding='utf-8')
     
     
     return degiro_data
